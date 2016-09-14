78bfe7a1fc80ae905d41bcd3d2058818321e643c
hemalrathod <rathod.hemal@gmail.com>
2016-09-14 17:46:45 +0530

    Added HomeController and index.jsp

---
+++ src/main/java/in/hrsoftwares/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package in.hrsoftwares.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller(value="/Home")
+public class HomeController {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String Index(){
+		return "index";
+	}
+	
+}
